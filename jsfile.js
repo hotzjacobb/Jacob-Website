4964f9cee00a27fcd5e4523d30c42b56e9c4c150
hotzjacobb <hotzjacobb@gmail.com>
2020-02-13 08:38:09 -0800

    fixed bug caused by adjusting value before calculating ratio

--- jsfile.js
+++ jsfile.js
@@ -16,21 +16,33 @@ function copyEmail() {
 function onClickProjects() {
     var shapeContainer = document.getElementById("shape-container")
     var shapes  = shapeContainer.children // Returns an HTMLCollection
+    console.log(.3567446397 / .135674356)
     for (shape of shapes) {
         if (shape.className !== "shape circle red") {
             // shape.style.display = "none"
             // potentially set display to none here during transiton
-            let xVector = Math.random() - 1
-            let yVector = Math.random() - 1
-            if (Math.abs(yVector) > Math.abs(xVector)) {   // get movement for one frame
-                xVector = 1
-                yVector = Math.round(yVector / xVector)
-            } else {
-                yVector = 1
-                xVector = Math.round(xVector / yVector)
-            }
+            var xVector = Math.random() - .5
+            var yVector = Math.random() - .5
             console.log(shape.className + "'s x-vec is " + xVector.toString())
-            console.log(shape.className + "'s y-vec is " + xVector.toString())
+            console.log(shape.className + "'s y-vec is " + yVector.toString())
+            if (Math.abs(yVector) > Math.abs(xVector)) {   // get movement for one frame
+                var ratio = (yVector * 1.0 / xVector)
+                console.log("The ratio before rounding is" + ratio)
+                ratio = Math.round(ratio)
+                console.log("The ratio after rounding is" + ratio)
+                yVector = (yVector > 0) ? ratio : (-1 * ratio)
+                xVector = (xVector > 0) ? 1 : -1
+            } else {
+                var ratio = (xVector * 1.0 / yVector)
+                console.log("The ratio before rounding is" + ratio)
+                ratio = Math.round(ratio)
+                console.log("The ratio after rounding is" + ratio)
+                xVector = (xVector > 0) ? ratio : (-1 * ratio)
+                yVector = (yVector > 0) ? 1 : -1
+            }   // The smaller of xVector and yVector is now 1 and the larger keeps the same
+                // the same ratio; will be used for movement across pixels
+            console.log(shape.className + "'s x-vec NOW is " + xVector.toString())
+            console.log(shape.className + "'s y-vec NOW is " + yVector.toString())
             var rect = shape.getBoundingClientRect()
             let windowWidth = window.innerWidth
             let windowHeight = window.innerHeight
@@ -48,6 +60,7 @@ function onClickProjects() {
             console.log("translate(" + tracerXCoord + "px, " + tracerYCoord + "px)")
             shape.style.transform = "translate(" + tracerXCoord + "px, " + tracerYCoord + "px)" // random translation
             let rotationAngle = Math.random() * 360
+            // TODO: figure out how to do two translations at a time
             //shape.style.transform = "rotate(" + rotationAngle.toString() + "deg)" // random rotation
             shape.style.opacity = 0
         }
