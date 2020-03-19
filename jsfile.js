ff90cead042604415592b461f7d71d202dd63a63
hotzjacobb <hotzjacobb@gmail.com>
2020-03-19 16:59:03 -0700

    mobile version implemented

--- jsfile.js
+++ jsfile.js
@@ -1,5 +1,25 @@
 // Created by Jacob Hotz 17 Dec. 2019
 
+
+// Called on page load to decide whether to show the mobile or desktop version
+
+window.onload = onLoadVersion
+
+function onLoadVersion() {
+    if (mobileCheck()) {
+        document.getElementById("mobile").style.display = "block"
+    } else {
+        document.getElementById("desktop").style.display = "block"
+    }
+}
+
+// credit to https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+function mobileCheck() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
 // makes the email icon disappear (replaced by text) and then reappear
 function copyEmail() {
     correctEmail()
@@ -121,39 +141,50 @@ function textClickedFinished(shape, displayedButton) {
 // and then creates the new div that is the module.
 function centredFinished(shapeToHide, btnClicked) {
     shapeToHide.removeEventListener("transitionend", centredFinished)
-    let module = document.createElement("DIV")
-    module.id = "module"
+    
     let colour
     if (shapeToHide.id === "blanchedalmond") { // necessary b/c of triangle
         colour = window.getComputedStyle(shapeToHide).getPropertyValue("border-bottom-color")
     } else {
         colour = window.getComputedStyle(shapeToHide).getPropertyValue("background-color")
     }
-    module.style.backgroundColor = 	colour  // module has the same colour as button's parent (shape)
+    // module.style.backgroundColor = colour  // module has the same colour as button's parent (shape)
+    displayModule(colour)
     let shapeContainer = document.getElementById("shape-container")
     shapeContainer.style.display = "none"
+
+    // // fill module with text and back button when anim. almost done
+    let module = document.getElementById("module")
+    moduleAnimDone(btnClicked, module, restoreInitialPage)
+}
+
+// Helper function that creates and displays the module
+function displayModule(colour) {
+    let module = document.createElement("DIV")
+    module.id = "module"
+    module.style.backgroundColor = colour
     document.body.appendChild(module)
     window.getComputedStyle(module).top; // this line forces a redraw; otherwise no anim.
     module.classList.add("expand")
-    // fill module with text and back button when anim. almost done
+    return module
+}
+
+// Helper function that waits and right as module finishes anim. prepares it
+function moduleAnimDone(btnClicked, module, cleanUpFunction) {
     setTimeout(function () {
         let backArrow = document.createElement("INPUT")
         backArrow.type = "image"
-        // backArrow.src = "./images/noun_back_1521731.png" // alternate back icon; by Besticons noun proj.
         backArrow.src = "./images/noun_back_878298.png"
         backArrow.alt = "Back"
-        backArrow.onclick = removeModule
+        backArrow.onclick = () => removeModule(cleanUpFunction);
         backArrow.id = "back-arrow"
         document.body.appendChild(backArrow)
-        getModuleContent(btnClicked, module)
+        getModuleContent(btnClicked, module) // get the appropriate content
     }, 750);
 }
 
-// Helper function to get the module's appropriate content
+// Helper function to get the module's appropriate content on desktop
 function getModuleContent(btnClicked, module) {
-    // let moduleContent = document.createElement("DIV")
-    // moduleContent.id = "content"
-    // module.appendChild(moduleContent)
     switch (btnClicked.storedInnerHTML) {
         case "Projects":
             let projHidden = document.getElementById("proj-hidden")
@@ -207,9 +238,9 @@ function deleteText(clickedButtonId) {
 // Triggers a brief animation before deleting the module
 // from the page. Places listener which upon completion
 // calls restoreInitialPage
-function removeModule() {
+function removeModule(callbackAfterAnim) {
     let module = document.getElementById("module")
-    module.addEventListener("transitionend", restoreInitialPage, {once: true})
+    module.addEventListener("transitionend", callbackAfterAnim, {once: true})
     module.classList.remove("expand") // first animate removal of module
     let backArrow = document.getElementById("back-arrow")
     backArrow.remove()
@@ -227,8 +258,6 @@ function restoreInitialPage() {
 
     let module = document.getElementById("module")
     module.remove()
-
-    // TODO: move module-content back to body and hide
 
     document.getElementById("header").style.display = "block"
 
@@ -254,6 +283,14 @@ function restoreInitialPage() {
     document.getElementById("footer").style.display = "grid"
 }
 
+function restoreInitialPageMobile() {
+
+    let module = document.getElementById("module")
+    module.remove()
+
+    let menu = document.getElementById("mobile-menu")
+    menu.style.display = "block"
+}
 
 // Helper function that maps the button to its correct shape
 // regardless of the parent eleement
@@ -319,6 +356,13 @@ function correctEmail() {
 // Mobile
 // function called when user chooses option from mobile menu
 function onClickMobile(btnClicked) {
+    btnClicked.storedInnerHTML = btnClicked.innerHTML
     let menu = document.getElementById("mobile-menu").style.display = "none"
-    
+    let module
+    if (btnClicked.innerHTML === "Projects") {   // special case; projects looks better in red
+        module = displayModule(window.getComputedStyle(document.getElementById("blog-mobile")).getPropertyValue("background-color"))
+    } else {
+        module = displayModule(window.getComputedStyle(btnClicked.parentElement).getPropertyValue("background-color")) // display module
+    }
+    moduleAnimDone(btnClicked, module, restoreInitialPageMobile)
 }
